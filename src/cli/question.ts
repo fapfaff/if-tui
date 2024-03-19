45fe32ce02986f1b74cdefefce4a9cbf7821231d
fapfaff <fabian-pfaff@outlook.de>
2024-03-19 19:11:02 +0000

    feat(src): add SingleChoiceQuestion

--- src/cli/question.ts
+++ src/cli/question.ts
@@ -1,3 +1,5 @@
+import {Choice} from '../types/choice';
+
 /**
  * Represents a question in a command-line interface.
  */
@@ -25,5 +27,27 @@ export class OpenTextQuestion extends Question {
     super(prompt);
     this.prompt = prompt;
     this.defaultValue = defaultValue;
+  }
+}
+
+/**
+ * Represents a single choice question.
+ */
+export class SingleChoiceQuestion extends Question {
+  prompt: string;
+  defaultValue: string | undefined;
+  choices: Choice[];
+
+  /**
+   * Creates a new instance of SingleChoiceQuestion.
+   * @param prompt The prompt for the question.
+   * @param choices The choices for the question.
+   * @param defaultValue The default value for the question (optional).
+   */
+  constructor(prompt: string, choices: Choice[], defaultValue?: string) {
+    super(prompt);
+    this.prompt = prompt;
+    this.defaultValue = defaultValue;
+    this.choices = choices;
   }
 }
