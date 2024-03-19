6c464a7c758cc3613c4bc4b0c8cd16c9c63f9cae
fapfaff <fabian-pfaff@outlook.de>
2024-03-19 13:51:14 +0000

    feat(src): add type guard for Answer

--- src/types/answer.ts
+++ src/types/answer.ts
@@ -1,1 +1,10 @@
 export type Answer = string | string[] | boolean | number;
+
+/**
+ * Checks if the provided answer is a string.
+ * @param answer - The answer to check.
+ * @returns True if the answer is a string, false otherwise.
+ */
+export function isStringAnswer(amswer: Answer): amswer is string {
+  return typeof amswer === 'string';
+}
