9aeea468f15fbeb260b0f44c96df9ca350d45164
fapfaff <fabian-pfaff@outlook.de>
2024-03-27 16:41:36 +0000

    feat(src): add sci and sci-o to the pipeline

--- src/__tests__/unit/cli/states/backend-state.test.ts
+++ src/__tests__/unit/cli/states/backend-state.test.ts
@@ -1,11 +1,65 @@
+import {CompositeState} from '../../../../cli/state';
 import {BackendState} from '../../../../cli/states/backend-state';
+import {HostingState} from '../../../../cli/states/hosting-state';
 import {TuiManager} from '../../../../cli/tui-manager';
 
 describe('BackendState', () => {
+  let context: CompositeState;
+  let componentPath: string[];
+  let backendState: BackendState;
+
+  beforeEach(() => {
+    context = new TuiManager();
+    componentPath = ['a', 'b'];
+    backendState = new BackendState(context, componentPath);
+    backendState.getManifestBuilder().setNodeAtPath(componentPath, {});
+  });
+
+  it('should create an instance of BackendState', () => {
+    expect(backendState).toBeInstanceOf(BackendState);
+  });
+
+  it('should add states to the queue', () => {
+    expect(backendState.stateQueue[0]).toEqual(expect.any(HostingState));
+  });
+
   it('should set the component path correctly', () => {
     const componentPath = ['backend'];
     const state = new BackendState(new TuiManager(), componentPath);
 
     expect(state.componentPath).toEqual(componentPath);
+  });
+
+  it('should add sci and sci-o to the initialization', () => {
+    const sciOPlugin = {
+      path: '@grnsft/if-plugins',
+      method: 'SciO',
+    };
+    const sciPlugin = {
+      path: '@grnsft/if-plugins',
+      method: 'Sci',
+      'global-config': {
+        'functional-unit-time': '5 minutes',
+      },
+    };
+
+    backendState.afterStateExecution();
+
+    const manifest = backendState.getManifestBuilder().build();
+    expect(manifest.initialize.plugins).toEqual(
+      expect.objectContaining({
+        'sci-operational': sciOPlugin,
+        sci: sciPlugin,
+      })
+    );
+  });
+
+  it('should add sci-operational and sci to the pipeline', () => {
+    backendState.afterStateExecution();
+
+    const manifest = backendState.getManifestBuilder().build();
+    expect(manifest.tree.children.a.children.b.pipeline).toEqual(
+      expect.arrayContaining(['sci-operational', 'sci'])
+    );
   });
 });
--- src/cli/states/backend-state.ts
+++ src/cli/states/backend-state.ts
@@ -17,4 +17,42 @@ export class BackendState extends CompositeState implements WithComponent {
     this.componentPath = componentPath;
     this.stateQueue.push(new HostingState(this, componentPath));
   }
+
+  async afterStateExecution(): Promise<void> {
+    this.addPlugins();
+    this.addPluginsToPipeline();
+  }
+
+  /**
+   * Adds sci-operational and sci plugins to the initialize section.
+   */
+  private async addPlugins(): Promise<void> {
+    const sciOPlugin = {
+      path: '@grnsft/if-plugins',
+      method: 'SciO',
+    };
+    const sciPlugin = {
+      path: '@grnsft/if-plugins',
+      method: 'Sci',
+      'global-config': {
+        'functional-unit-time': '5 minutes',
+      },
+    };
+
+    this.getManifestBuilder().addPlugin('sci-operational', sciOPlugin);
+    this.getManifestBuilder().addPlugin('sci', sciPlugin);
+  }
+
+  /**
+   * Adds sci-operational and sci plugins to the pipeline of the current node in the manifest builder.
+   * @returns A Promise that resolves when the plugins are added successfully.
+   * @throws An error if the node is not found.
+   */
+  private async addPluginsToPipeline(): Promise<void> {
+    const node = this.getManifestBuilder().getNodeAtPath(this.componentPath);
+    if (!node) throw new Error('Node not found');
+    if (!node.pipeline) node.pipeline = [];
+    node.pipeline.push('sci-operational');
+    node.pipeline.push('sci');
+  }
 }
