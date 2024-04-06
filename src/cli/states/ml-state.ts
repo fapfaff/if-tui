5c2ee370b1ee57c722b460ce37b62c909670c35d
fapfaff <fabian-pfaff@outlook.de>
2024-04-06 10:23:35 +0000

    feat(src): add MlState
     - adds data, inference and training components

--- src/cli/states/ml-state.ts
+++ src/cli/states/ml-state.ts
@@ -3,4 +3,14 @@ import {CompositeState} from '../state';
 /**
  * State for configuring a machine learning project.
  */
-export class MlState extends CompositeState {}
+export class MlState extends CompositeState {
+  constructor(context: CompositeState) {
+    super(context);
+  }
+
+  async beforeStateExecution(): Promise<void> {
+    this.getManifestBuilder().setNodeAtPath(['data'], {});
+    this.getManifestBuilder().setNodeAtPath(['training'], {});
+    this.getManifestBuilder().setNodeAtPath(['inference'], {});
+  }
+}
