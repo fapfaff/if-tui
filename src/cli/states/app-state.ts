a83888fca088b3c17c41faef1438c930f889d6e7
fapfaff <fabian-pfaff@outlook.de>
2024-04-06 10:16:57 +0000

    feat(src): add AppState
     - adds build and client components
     - enqueues HasBackendState

--- src/cli/states/app-state.ts
+++ src/cli/states/app-state.ts
@@ -1,6 +1,16 @@
 import {CompositeState} from '../state';
+import {HasBackendState} from './has-backend-state';
 
 /**
  * State for configuring a mobile or desktop app project.
  */
-export class AppState extends CompositeState {}
+export class AppState extends CompositeState {
+  constructor(context: CompositeState) {
+    super(context);
+    this.stateQueue.push(new HasBackendState(this, []));
+  }
+  async beforeStateExecution(): Promise<void> {
+    this.getManifestBuilder().setNodeAtPath(['build'], {});
+    this.getManifestBuilder().setNodeAtPath(['client'], {});
+  }
+}
