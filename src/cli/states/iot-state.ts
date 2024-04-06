a1a8f981028da6a2a6f70853cd06f9bf80ab1669
fapfaff <fabian-pfaff@outlook.de>
2024-04-06 10:05:18 +0000

    feat(src): add IOT state
     - adds hardware and firmware components
     - enqueues HasBackendState

--- src/cli/states/iot-state.ts
+++ src/cli/states/iot-state.ts
@@ -1,6 +1,17 @@
 import {CompositeState} from '../state';
+import {HasBackendState} from './has-backend-state';
 
 /**
  * State for configuring a iot project.
  */
-export class IotState extends CompositeState {}
+export class IotState extends CompositeState {
+  constructor(context: CompositeState) {
+    super(context);
+    this.stateQueue.push(new HasBackendState(this, []));
+  }
+
+  async beforeStateExecution(): Promise<void> {
+    this.getManifestBuilder().setNodeAtPath(['hardware'], {});
+    this.getManifestBuilder().setNodeAtPath(['firmware'], {});
+  }
+}
